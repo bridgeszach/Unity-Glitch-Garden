e1f52a9e7a48a641d1fb79e0ff38b095fcd4b00e
Zach Bridges <56745560+bridgeszach@users.noreply.github.com>
2020-05-05 13:30:58 -0400

    Initial Commit

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] float minDelay = 1f;
+    [SerializeField] float maxDelay = 5f;
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            SpawnAttacker();
+        }        
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+        
+    void Update()
+    {
+        
+    }
+}
